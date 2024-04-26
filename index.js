248e0d5fbaa276643b91e1ca43503a8c8c50aaa8
repl.it user <replituser@example.com>
2024-04-26 22:49:23 +0200

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.listen(() => console.log('Bird YT'));
+app.listen(() => console.log('giveawaybot'));
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let fs = require('fs');
@@ -6,13 +16,13 @@ const { prefix, token } = require('./config.json');
 client.on('ready', () => {
   console.log(`Bot tag: ${client.user.tag}`);
   console.log(`Guilds: ${client.guilds.cache.size}`);
-  client.user.setActivity(`with ${prefix}giveaway & ${prefix}help`, { type: 'PLAYING' });
+  client.user.setActivity(`with ${prefix}giveaway`, { type: 'PLAYING' });
 });
 
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles) {
+for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
 
   client.commands.set(command.name, command);
@@ -21,11 +31,11 @@ for(const file of commandFiles) {
 client.on('message', async message => {
   if (message.content.startsWith(`${prefix}`)) {
     let file_name = `${message.content.split(' ')[0].replace(prefix, '')}.js`;
-    if(!fs.existsSync('./commands/' + file_name)) return undefined;
-    if(fs.existsSync('./commands/' + file_name)) {
+    if (!fs.existsSync('./commands/' + file_name)) return undefined;
+    if (fs.existsSync('./commands/' + file_name)) {
       client.commands.get(file_name.replace('.js', '')).execute(client, message);
     }
   }
 });
 
-client.login(token);
+client.login(process.env.token);
